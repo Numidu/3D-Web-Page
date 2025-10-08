3d9864b47f5b52a55e3d67055625e9f24571aa63
Numidu <dnumidu@gmail.com>
2025-10-08 23:21:25 +0530

    configTailwandcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
